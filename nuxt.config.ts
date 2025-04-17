f00ae38ca4500136b8dd1c170db2feadc389ab00
Juan Carlos Quintero <juanquinterodj@gmail.com>
2025-04-17 11:23:20 -0300

    Installed nuxt, eslint and tailwind 4

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="text-red-500 font-bold text-4xl">Home</h1>
+  </div>
+</template>
