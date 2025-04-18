6732d31bc10e4ac7cd1f7eebfea48193d3e653b2
Juan Carlos Quintero <juanquinterodj@gmail.com>
2025-04-18 14:29:22 -0300

    Installed and configured shadcn-vue. Added success color to main and variant for button component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,20 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "shadcn-nuxt"],
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1 class="text-red-500 font-bold text-4xl">Home</h1>
+  <div
+    class="flex flex-col max-w-2/3 justify-center items-center m-auto space-y-4"
+  >
+    <h1 class="text-red-500 font-bold text-4xl my-2">Home</h1>
+
+    <Button variant="default" as-child>
+      <NuxtLink to="/your-page">Click me</NuxtLink>
+    </Button>
+
+    <Button variant="success" as-child>
+      <NuxtLink to="/your-page">Click me</NuxtLink>
+    </Button>
   </div>
 </template>
